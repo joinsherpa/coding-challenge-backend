236828ed5db2f61bab87592f8cdf18228d203c87
Iliyass Hamza <iliyass.hamza@gmail.com>
2022-04-13 11:29:35 +0000

    add pagination, from get param

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express"
 import { Request, Response } from "express"
 import {Server} from "http";
+import { MoreThan } from "typeorm";
 import {initializeDB} from "./database";
 import { Event } from "./entity/Event";
 
 const sleep = (ms: number): Promise<void> => new Promise((res) => setTimeout(res, ms))
 
+interface EventsListingQuery {
+    page:number
+    pageSize: number
+    from: number
+    until: number
+}
 export const start = async (): Promise<Server> => new Promise(async (resolve, reject) => {
     try {
         const port = 4040
@@ -14,12 +21,20 @@ export const start = async (): Promise<Server> => new Promise(async (resolve, re
         if( !db) {
             reject("Database not connected")
         }
-        app.get('/events', async (req:Request, res:Response) => {
+        app.get('/events', async (req:Request<any, any, any, EventsListingQuery>, res:Response) => {
+            const pageSize = req.query.pageSize || 10
+            const page = req.query.page || 1
+            const from = req.query.from || +new Date()
             const events = await db?.getRepository(Event).find({
+                where: {
+                    date: MoreThan(from)
+                },
                 relations: {
                     location: true,
                     organizer: true
-                }
+                },
+                take: pageSize,
+                skip: (page - 1 ) * pageSize
             })
             res.json({ results: events })
         })
