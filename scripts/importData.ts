b068f7b3a9ae2dca0c1ad916ade90998d75a016c
mado <mohammad.zbaker@gmail.com>
2022-07-19 06:50:52 -0400

    feat(importData): check if enough data exists before inserting

--- scripts/importData.ts
+++ scripts/importData.ts
@@ -15,15 +15,21 @@ interface Event {
 
 export const importData = async () => {
     for (const e of <Event[]>events) {
-        await queryDB('INSERT OR IGNORE INTO users (name) VALUES (?)', [e.organizer.name]);
-        await queryDB('INSERT OR IGNORE INTO events (name, location, isOutside, date, organizer) VALUES (?, ?, ?, ?, ?)',
-            [
-                e.name,
-                e.location,
-                e.isOutside,
-                e.date,
-                e.organizer.name
-            ]);
+        if (e.organizer.name) {
+            await queryDB('INSERT OR IGNORE INTO users (name) VALUES (?)', [e.organizer.name]);
+            if (e.name && e.location && typeof e.isOutside === 'boolean') {
+                await queryDB('INSERT OR IGNORE INTO events (name, location, isOutside, date, organizer) VALUES (?, ?, ?, ?, ?)',
+                    [
+                        e.name,
+                        e.location,
+                        e.isOutside,
+                        e.date,
+                        e.organizer.name
+                    ]);
+            }
+        }
+
+
     }
     return 'done';
 }
